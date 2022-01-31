d2c24f4706c74a6c358e48a7863371c22a3bf797
Chris Cazaly <chris.cazaly@coop.co.uk>
2022-01-31 13:52:44 +0000

    create tax calculator for petrol cars

--- exercises/javascript/test/default-tax-calculator.js
+++ exercises/javascript/test/default-tax-calculator.js
@@ -8,8 +8,22 @@ let DefaultTaxCalculator = class DefaultTaxCalculator extends TaxCalculator {
     super();
   }
   calculateTax(vehicle) {
-    return -1;
-  }
+    const co2 = vehicle.co2Emissions;
+    if (co2 <= 0) return 0;
+    else if (co2 <= 50) return 10
+    else if (co2 <= 75) return 25
+    else if (co2 <= 90) return 105
+    else if (co2 <= 100) return 125
+    else if (co2 <= 110) return 145
+    else if (co2 <= 130) return 165
+    else if (co2 <= 150) return 205
+    else if (co2 <= 170) return 515
+    else if (co2 <= 190) return 830
+    else if (co2 <= 225) return 1240
+    else if (co2 <= 255) return 1760
+    else return 2070
+
+}
 }
 
 module.exports = {
