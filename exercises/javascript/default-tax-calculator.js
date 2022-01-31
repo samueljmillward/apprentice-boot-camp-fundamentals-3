ea0157ee9553df478c4139917f0a9cc807f5dc7c
Samuel Millward <samuel.millward@BRI50131L.local>
2022-01-31 15:04:46 +0000

    Add chained ternary

--- exercises/javascript/default-tax-calculator.js
+++ exercises/javascript/default-tax-calculator.js
@@ -1,11 +1,27 @@
-const { TaxCalculator } = require('../tax-calculator');
+const { TaxCalculator } = require('./tax-calculator');
 
 let DefaultTaxCalculator = class DefaultTaxCalculator extends TaxCalculator {
   constructor() {
     super();
   }
+
   calculateTax(vehicle) {
-    return -1;
+    const co2 = vehicle.co2Emissions;
+
+    return co2 == 0 ? 0
+        : co2 <= 50 ? 10 
+        : co2 <= 75 ? 25 
+        : co2 <= 90 ? 105
+        : co2 <= 100 ? 125
+        : co2 <= 110 ? 145
+        : co2 <= 130 ? 165
+        : co2 <= 150 ? 205
+        : co2 <= 170 ? 515
+        : co2 <= 190 ? 830
+        : co2 <= 225 ? 1240
+        : co2 <= 255 ? 1760
+        : co2 >= 256 ? 2070
+        : null
   }
 }
 
