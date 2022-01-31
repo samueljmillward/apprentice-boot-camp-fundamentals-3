9c445334625baf5b052697f5d9427f5f2aad3a8c
Chris Cazaly <chris.cazaly@coop.co.uk>
2022-01-31 16:22:25 +0000

    Add alternative fuel calculations

--- exercises/javascript/test/default-tax-calculator.js
+++ exercises/javascript/test/default-tax-calculator.js
@@ -1,3 +1,4 @@
+const { FuelType } = require('../fuel-type');
 const { TaxCalculator } = require('../tax-calculator');
 
 /**
@@ -9,19 +10,38 @@ let DefaultTaxCalculator = class DefaultTaxCalculator extends TaxCalculator {
   }
   calculateTax(vehicle) {
     const co2 = vehicle.co2Emissions;
-    if (co2 <= 0) return 0;
-    else if (co2 <= 50) return 10
-    else if (co2 <= 75) return 25
-    else if (co2 <= 90) return 105
-    else if (co2 <= 100) return 125
-    else if (co2 <= 110) return 145
-    else if (co2 <= 130) return 165
-    else if (co2 <= 150) return 205
-    else if (co2 <= 170) return 515
-    else if (co2 <= 190) return 830
-    else if (co2 <= 225) return 1240
-    else if (co2 <= 255) return 1760
-    else return 2070
+    const fuelType = vehicle.fuelType;
+
+    if (fuelType == FuelType.PETROL) {
+        if (co2 <= 0) return 0;
+        else if (co2 <= 50) return 10
+        else if (co2 <= 75) return 25
+        else if (co2 <= 90) return 105
+        else if (co2 <= 100) return 125
+        else if (co2 <= 110) return 145
+        else if (co2 <= 130) return 165
+        else if (co2 <= 150) return 205
+        else if (co2 <= 170) return 515
+        else if (co2 <= 190) return 830
+        else if (co2 <= 225) return 1240
+        else if (co2 <= 255) return 1760
+        else return 2070
+    } else if (fuelType == FuelType.ALTERNATIVE_FUEL) {
+        if (co2 <= 50) return 0;
+        else if (co2 <= 75) return 15
+        else if (co2 <= 90) return 95
+        else if (co2 <= 100) return 115
+        else if (co2 <= 110) return 135
+        else if (co2 <= 130) return 155
+        else if (co2 <= 150) return 195
+        else if (co2 <= 170) return 505
+        else if (co2 <= 190) return 820
+        else if (co2 <= 225) return 1230
+        else if (co2 <= 255) return 1750
+        else return 2060
+    } else if (fuelType == FuelType.ELECTRIC) {
+        return 0
+    }
 
 }
 }
